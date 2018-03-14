622dad96511a1b1898bb943e687d585aa37227f7
EdyLG <edy17496@gmail.com>
2018-03-14 20:01:41 +0100

    Main con lo pasos para poder hacer el commit, del proyecto al repos.
    Signed-off-by: EdyLG <edy17496@gmail.com>

---
+++ nanoGamesStudents/src/mainGit.java
@@ -0,0 +1,15 @@
+
+public class mainGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Para poder hacer el commit al repositorio:
+		 * 	1º Proyecto->Team->Syncronize Workspace->Commit y ya se hace el commit
+		 * 	NOTA: Añadir algun comentario, para luego poder entenderlo  se queda mas jamon
+		 *  <3
+		 */
+
+	}
+
+}
